b5382e4bd56098629f45e01a33050addbf62b116
jameygleason <james.p.gleason@gmail.com>
2020-06-14 12:01:49 -0400

    watcher and copy packages to node_module working

--- watcher.js
+++ watcher.js
@@ -6,14 +6,12 @@ import child_process from 'child_process'
 chokidar.watch(['./**/*.graphql', '!./__tempo__']).on('change', event => {
   console.log(chalk.blue(`[watcher] Change in ${event}`))
 
-  const dev = process.env.NODE_ENV === 'development'
-  // const command = dev
-  //   ? 'cross-env cd packages/tempo && go build && ./tempo'
-  //   : 'cross-env cd node_modules/tempo/tempo'
-
   child_process.exec(
-    'go build && tempo',
-    { cwd: `${path.join(process.cwd(), 'packages', 'tempo')}` },
+    `cp -r ${path.join(process.cwd(), 'packages', 'tempo')} ${path.join(
+      process.cwd(),
+      'node_modules',
+    )} && go build && tempo`,
+    { cwd: `${path.join(process.cwd(), 'node_modules', 'tempo')}` },
     (error, stdout, stderr) => {
       if (error) {
         console.error(`error: ${error.message}`)
