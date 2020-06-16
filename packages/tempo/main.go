b3aface17163e0d5ce9673b19d6270f362e1fb69
jameygleason <james.p.gleason@gmail.com>
2020-06-16 18:12:48 -0400

    added registerapi copy task

--- packages/tempo/main.go
+++ packages/tempo/main.go
@@ -115,50 +115,49 @@ func main() {
 
 	//* Below is code for registerAPI and is a [WIP]
 	//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	/**
-	 * Query and mutation slices
-	 */
-	mutation := []string{}
-	query := []string{}
+	// /**
+	//  * Query and mutation slices
+	//  */
+	// mutation := []string{}
+	// query := []string{}
 
-	/**
-	 * Read JS files and store them to slices
-	 */
-	for _, file := range jsFiles {
-		// TODO: instead of excluding things that aren't resolvers, just get the resolvers
-		isResolver := false
-		if !strings.Contains(file, "registerAPI") && !strings.Contains(file, "typeDefs") && !strings.Contains(file, "scalars") && !strings.Contains(file, "utils") && !strings.Contains(file, "lib") && !strings.Contains(file, "model") && !strings.Contains(file, "server") {
-			isResolver = true
-		}
+	// /**
+	//  * Read JS files and store them to slices
+	//  */
+	// for _, file := range jsFiles {
+	// 	// TODO: instead of excluding things that aren't resolvers, just get the resolvers
+	// 	isResolver := false
+	// 	if !strings.Contains(file, "registerAPI") && !strings.Contains(file, "typeDefs") && !strings.Contains(file, "scalars") && !strings.Contains(file, "utils") && !strings.Contains(file, "lib") && !strings.Contains(file, "model") && !strings.Contains(file, "server") {
+	// 		isResolver = true
+	// 	}
 
-		if !isResolver {
-			continue
-		}
+	// 	if !isResolver {
+	// 		continue
+	// 	}
 
-		if strings.Contains(file, "scalars") {
-			// Write in the scaler
-		}
+	// 	if strings.Contains(file, "scalars") {
+	// 		// Write in the scaler
+	// 	}
 
-		/**
-		 * Split mutations and queries into there own slices
-		 */
+	// 	/**
+	// 	 * Split mutations and queries into there own slices
+	// 	 */
+	// 	path := fmt.Sprintf("%s", strings.Split(file, "src")[:2][1])
+	// 	_, file := filepath.Split(path)
+	// 	// dirName := file.Join
+	// 	functionName := strings.TrimSuffix(file, filepath.Ext(file))
 
-		path := fmt.Sprintf("%s", strings.Split(file, "src")[:2][1])
-		_, file := filepath.Split(path)
-		// dirName := file.Join
-		functionName := strings.TrimSuffix(file, filepath.Ext(file))
+	// 	if strings.Contains(file, "mutation") {
+	// 		mutation = append(mutation, path)
+	// 	}
 
-		if strings.Contains(file, "mutation") {
-			mutation = append(mutation, path)
-		}
+	// 	if strings.Contains(file, "query") {
+	// 		query = append(query, path)
+	// 	}
 
-		if strings.Contains(file, "query") {
-			query = append(query, path)
-		}
-
-		// DELETE - log path assets
-		// TODO: figure out how to write register api
-		fmt.Printf("\npath: ../src%s\nfunction: %s\n", filepath.ToSlash(path), functionName)
-		// DELETE
+	// 	// DELETE - log path assets
+	// 	// TODO: figure out how to write register api
+	// 	fmt.Printf("\npath: ../src%s\nfunction: %s\n", filepath.ToSlash(path), functionName)
+	// 	// DELETE
 	}
 }
--- packages/tempo/registerAPI.js
+++ packages/tempo/registerAPI.js
@@ -1,10 +1,10 @@
 // Resolvers
-import testConnection from '../../src/base/query/testConnection.js'
-import populateDb from '../../src/base/mutation/populateDb.js'
-import resetDb from '../../src/base/mutation/resetDb.js'
-import createUser from '../../src/user/mutation/createUser.js'
-import allUsers from '../../src/user/query/allUsers.js'
-import singleUser from '../../src/user/query/singleUser.js'
+import testConnection from '../src/base/query/testConnection.js'
+import populateDb from '../src/base/mutation/populateDb.js'
+import resetDb from '../src/base/mutation/resetDb.js'
+import createUser from '../src/user/mutation/createUser.js'
+import allUsers from '../src/user/query/allUsers.js'
+import singleUser from '../src/user/query/singleUser.js'
 
 // Scalars
 import scalar from 'tempo/scalars'
