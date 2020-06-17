e70b3f75a6ac6fc46d406bbc9d6b8e3b5f51b7db
jameygleason <james.p.gleason@gmail.com>
2020-06-16 20:19:40 -0400

    import strings complete

--- packages/tempo/main.go
+++ packages/tempo/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -114,59 +115,69 @@ func main() {
 
 	//* Below is code for registerAPI and is a [WIP]
 	//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	// /**
-	//  * Query and mutation slices
-	//  */
-	// var resolvers = make(map[string]string)
+	/**
+	 * Query and mutation slices
+	 */
+	var resolvers = make(map[string]string)
 
-	// /**
-	//  * Read JS files and store them to slices
-	//  */
-	// for _, file := range jsFiles {
-	// 	// TODO: instead of excluding things that aren't resolvers, just get the resolvers
-	// 	isResolver := false
-	// 	if !strings.Contains(file, "registerAPI") && !strings.Contains(file, "typeDefs") && !strings.Contains(file, "scalars") && !strings.Contains(file, "utils") && !strings.Contains(file, "lib") && !strings.Contains(file, "model") && !strings.Contains(file, "server") {
-	// 		isResolver = true
-	// 	}
+	/**
+	 * Read JS files and store them to slices
+	 */
+	for _, file := range jsFiles {
+		// TODO: instead of excluding things that aren't resolvers, just get the resolvers
+		isResolver := false
+		if !strings.Contains(file, "registerAPI") && !strings.Contains(file, "typeDefs") && !strings.Contains(file, "scalars") && !strings.Contains(file, "utils") && !strings.Contains(file, "lib") && !strings.Contains(file, "model") && !strings.Contains(file, "server") {
+			isResolver = true
+		}
 
-	// 	if !isResolver {
-	// 		continue
-	// 	}
+		if !isResolver {
+			continue
+		}
 
-	// 	if strings.Contains(file, "scalars") {
-	// 		// Write in the scaler
-	// 	}
+		if strings.Contains(file, "scalars") {
+			// Write in the scaler
+		}
 
-	// 	/**
-	// 	 * Split mutations and queries into there own slices
-	// 	 */
-	// 	path := fmt.Sprintf("%s", strings.Split(file, "src")[:2][1])
-	// 	_, file := filepath.Split(path)
-	// 	// dirName := file.Join
-	// 	functionName := strings.TrimSuffix(file, filepath.Ext(file))
-	// 	resolvers[functionName] = filepath.ToSlash(path)
-	// }
+		/**
+		 * Split mutations and queries into there own slices
+		 */
+		path := fmt.Sprintf("%s", strings.Split(file, "src")[:2][1])
+		_, file := filepath.Split(path)
+		// dirName := file.Join
+		functionName := strings.TrimSuffix(file, filepath.Ext(file))
+		resolvers[functionName] = filepath.ToSlash(path)
+	}
 
-	// /**
-	//  *
-	//  */
-	// var mutation = make(map[string]string)
-	// var query = make(map[string]string)
+	/**
+	 * Build imports
+	 */
+	var output = ""
+	var mutation = []string{}
+	var query = []string{}
 
-	// for functionName, path := range resolvers {
-	// 	if strings.Contains(path, "mutation") {
-	// 		mutation[functionName] = filepath.ToSlash(path)
-	// 	}
+	for functionName, path := range resolvers {
+		imports := fmt.Sprintf("import %s from \"../src%s\";\n", functionName, filepath.ToSlash(path))
 
-	// 	if strings.Contains(path, "query") {
-	// 		query[functionName] = filepath.ToSlash(path)
-	// 	}
+		output = output + imports
+	}
 
-	// 	// DELETE - log path assets
-	// 	// TODO: figure out how to write register api
-	// 	fmt.Printf("\npath: ../src%s\nfunction: %s\n", filepath.ToSlash(path), functionName)
-	// 	// DELETE
-	// }
+	fmt.Println(output)
+
+	for functionName, path := range resolvers {
+		if strings.Contains(path, "mutation") {
+			mutation = append(mutation, functionName)
+		}
+
+		if strings.Contains(path, "query") {
+			query = append(query, functionName)
+		}
+	}
+
+	// DELETE - log path assets
+	// TODO: figure out how to write register api
+	fmt.Printf("function: %s\n", mutation)
+	fmt.Printf("function: %s\n", query)
+	// DELETE
 
 	// fmt.Println(mutation)
 	// fmt.Println(query)
