3262f2d9b3ace2fe402fc214802f37c0da1706bf
jameygleason <james.p.gleason@gmail.com>
2020-07-19 12:42:26 -0400

    removed example dir - duplication of root src

--- examples/basic/src/users/query/getAllUsers.js
+++
@@ -1,3 +0,0 @@
-module.exports = function getAllUser() {
-  return [{ name: "James", email: "james@mail.com" }];
-}
--- examples/basic/src/users/query/getSingleUser.js
+++
@@ -1,3 +0,0 @@
-module.exports = function getSingleUser() {
-  return { name: 'James', email: 'james@mail.com' }
-}
