33c9fb790c93a7c12cdb7f4e4436f95e103d2656
jameygleason <james.p.gleason@gmail.com>
2020-07-22 19:41:02 -0400

    added package dev scripts

--- __tempo__/watcher.js
+++ __tempo__/watcher.js
@@ -6,18 +6,18 @@ import child_process from 'child_process'
 
 const tempoDevEnv = path.join(process.cwd(), '__tempo__', 'package')
 const dist = path.join(process.cwd(), '__tempo__', 'tempo')
-const buildDir = path.join(process.cwd(), '__tempo__')
 
 function run() {
-  if (!fs.existsSync(buildDir)) {
-    fs.mkdirSync(buildDir)
+  if (process.env.PACKAGE_DEV) {
+    if (!fs.existsSync(dist)) {
+      copyDevToDist()
+    }
+
+    child_process.execSync('go build && tempo', { cwd: dist }, cb)
+    return
   }
 
-  if (!fs.existsSync(dist)) {
-    copyDevToDist()
-  }
-
-  child_process.execSync('go build && tempo', { cwd: dist }, cb)
+  child_process.execSync('tempo', { cwd: dist }, cb)
 }
 
 /**
@@ -30,10 +30,6 @@ chokidar
   .on('ready', () => {
     run()
   })
-  // .on('add', path => {
-  //   console.log(chalk.blue(`[tempo] Added file ${path}`))
-  //   run()
-  // })
   .on('addDir', path => {
     console.log(chalk.blue(`[tempo] Added directory ${path}`))
     run()
@@ -57,25 +53,26 @@ chokidar
 
 /**
  * For package dev
- *
- * Copy packages in packages dir into node modules
- * TODO: Find a better dev workflow
  */
-function copyDevToDist() {
-  if (!fs.existsSync(dist)) {
-    fs.mkdirSync(dist)
+let copyDevToDist
+
+if (process.env.PACKAGE_DEV) {
+  copyDevToDist = () => {
+    if (!fs.existsSync(dist)) {
+      fs.mkdirSync(dist)
+    }
+
+    child_process.execSync(`cp ${path.join(tempoDevEnv, 'main.go')} ${dist}`, cb)
   }
 
-  child_process.execSync(`cp ${path.join(tempoDevEnv, 'main.go')} ${dist}`, cb)
+  chokidar
+    .watch('./__tempo__/package/**/*')
+    .on('change', event => {
+      console.log(chalk.blue(`[tempo] Change in ${event}`))
+      copyDevToDist()
+      run()
+    })
 }
-
-chokidar
-  .watch('./__tempo__/package/**/*')
-  .on('change', event => {
-    console.log(chalk.blue(`[tempo] Change in ${event}`))
-    copyDevToDist()
-    run()
-  })
 
 /**
  * Callback for errors and stdout
