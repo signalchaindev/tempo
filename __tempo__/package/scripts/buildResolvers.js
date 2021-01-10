17d7cd9759dc57da311063d2cefea19e09204108
jameygleason <james.p.gleason@gmail.com>
2021-01-10 10:25:02 -0500

    refactored build scripts into separate dirs

--- __tempo__/package/scripts/buildResolvers.js
+++ __tempo__/package/scripts/buildResolvers.js
@@ -1,6 +1,6 @@
 import path from 'path'
-import { run } from './builder.js'
-import { rimraf } from './utils/rimraf.js'
+import { run } from '../builder.js'
+import { rimraf } from '../../utils/rimraf.js'
 
 const buildDir = path.join(process.cwd(), '__tempo__', 'build')
 
